79d1f6add034b1d328a5a5b8f792ae1feba37def
Maxence Aïci <maxence_aici@yahoo.fr>
2017-09-25 11:51:39 -0600

    Removed old frontend

--- public/app/resources/debt.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    'use strict';
-
-    angular.module('app')
-        .factory('Debt', ['$resource', function($resource){
-            return $resource('/api/me/debts');
-        }])
-})();
