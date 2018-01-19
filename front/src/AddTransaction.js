a55d870181b738f063d7af77b30aca88b39116f0
Dany Badr <badr.dany@gmail.com>
2018-01-19 21:00:34 +0100

    Revert "AddTransaction as Controlled Component"
    This reverts commit a6740be92bd8552532a40cebdfa381a1ad9683c8.

--- front/src/AddTransaction.js
+++ front/src/AddTransaction.js
@@ -2,103 +2,32 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
-class AddTransaction extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      paidBy: '1',
-      description: 'Yolo',
-      amount: 10,
-      paidFor: []
-    };
-
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleMultipleSelectChange = this.handleMultipleSelectChange.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const name = target.name;
-    const value = target.value;
-    console.log('new value is '+value);
-    this.setState({
-      [name]: value
-    });
-  }
-
-  handleMultipleSelectChange(event) {
-    const target = event.target;
-    const name = target.name;
-    let values = [...event.target.options]
-                  .filter(option => option.selected)
-                  .map(option => option.value);
-    this.setState({
-      [name]: values
-    });
-  }
-
-  render() {
-    const { data: { loading, user } } = this.props;
-    
-    if (!loading) {    
-      return (
-        <div>
-          <select name="paidBy"
-                  value={this.state.paidBy}
-                  onChange={this.handleInputChange}
-          >
-            <option key={user.id}
-                    value={user.id}
-            >
-              {user.firstName} {user.lastName}
-            </option>
-            { user.friends.map(friend => (
-              <option value={friend.id}
-                      key={friend.id}
-              >
-                {friend.firstName} {friend.lastName}
-              </option>
-            ))}
-          </select>
-          <input type="text"
-                  name="description"
-                  value={this.state.description}
-                  onChange={this.handleInputChange}
-                  required
-          />
-          <input type="number"
-                  name="amount"
-                  value={this.state.amount}
-                  onChange={this.handleInputChange}
-                  required
-          />
-          <select name="paidFor"
-                  value={this.state.paidFor}
-                  onChange={this.handleMultipleSelectChange}
-                  multiple
-          >
-            <option key={user.id}
-                    value={user.id}
-            >
-              {user.firstName} {user.lastName}
-            </option>
-            { user.friends.map(friend => (
-              <option value={friend.id}
-                      key={friend.id}
-              >
-                {friend.firstName} {friend.lastName}
-              </option>
-            ))}
-          </select>
-          <button type="submit">Ajouter une dépense</button>
-        </div>
-      )
-    }
-  
+const AddTransaction = ({data: {loading, user}}) => {
+  if (!loading) {
     return (
-      <div>Loading</div>
+      <div>
+        <select name="paidBy">
+          <option value={user.id}>{user.firstName} {user.lastName}</option>
+          { user.friends.map(friend => (
+            <option value={friend.id}>{friend.firstName} {friend.lastName}</option>
+          ))}
+        </select>
+        <input type="text" required/>
+        <input type="number" required/>
+        <select name="paidFor" multiple>
+          <option value={user.id}>{user.firstName} {user.lastName}</option>
+          { user.friends.map(friend => (
+            <option value={friend.id}>{friend.firstName} {friend.lastName}</option>
+          ))}
+        </select>
+        <button type="submit">Ajouter une dépense</button>
+      </div>
     )
   }
+
+  return (
+    <div>Loading</div>
+  )
 }
 
 export const appQuery = gql`
